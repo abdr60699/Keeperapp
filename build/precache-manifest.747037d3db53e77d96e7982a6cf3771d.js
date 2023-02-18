a5bd0130e27995aaaf4f39933e33a850dda2ef5e
Abdul Rahman <abdr60699@gmail.com>
2023-02-18 20:57:34 +0530

    Insert build

---
+++ build/precache-manifest.747037d3db53e77d96e7982a6cf3771d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ffcfcdfdf281f96e72a2d9e8969024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "a710656e046a579911ed",
+    "url": "/static/js/main.c517671c.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
